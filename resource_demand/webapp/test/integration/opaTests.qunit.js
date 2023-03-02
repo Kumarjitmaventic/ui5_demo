0f97621cdfcd19155130331832e924dcdc4018a8
Kumarjitmaventic <103119074+Kumarjitmaventic@users.noreply.github.com>
2023-03-02 18:10:05 +0530

    resource demand

---
+++ resource_demand/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["demo/demand/resourcedemand/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ resource_demand/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"demodemand/resource_demand/test/unit/controller/Home.controller"
+], function () {
+	"use strict";
+});
